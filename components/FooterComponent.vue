07bf1b34c45cc5c2e2612ffa76f515786199a2b0
Eva-Kravets <71512957+Eva-Kravets@users.noreply.github.com>
2022-02-05 14:47:38 +0300

    Add files via upload

---
+++ components/FooterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>Кравец Ева группа 201-322</footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+}
+</script>
+
+<style>
+footer {
+  background: black;
+  padding: 20px;
+  color: white;
+  text-align: center;
+}
+</style>
